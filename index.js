040bc8b84705ac008c83d08713d684d5a509e4a8
Devin <devingarcia5423@gmail.com>
2022-05-16 17:48:37 -0400

    commit tree ec355770981b63c4b2c5b0e26c6f86bcfc930751

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// function saturdayFun(act = "roller-skate") {
+//     const saturdayInner = "This Saturday, I want to";
+//     return function() {
+//             return `${part1} ${act}!`
+//     };
+// };
+// console.log(saturdayFun())
+function saturdayFun(act = 'roller-skate') {
+    const part1 = 'This Saturday, I want to';
+    const innerFunction = function() {
+        return`${part1} ${act}!`
+    };
+    return innerFunction();
+};
+
+function mondayWork(work = "go to the office") {
+    const part1 = "This Monday, I will"
+    const innerFunction = function () {
+        return `${part1} ${work}.`
+    };
+    return innerFunction()
+}
+
+function wrapAdjective(flair = "*") {
+    const part1 = "You are"
+    return function (adj = "special") {
+        return `${part1} ${flair}${adj}${flair}!`
+    }
+    // return innerFunction()
+}
+
+console.log(wrapAdjective())
